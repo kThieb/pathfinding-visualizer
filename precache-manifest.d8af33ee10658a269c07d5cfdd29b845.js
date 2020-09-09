9739927b207f80d426c32c6433dc0ea9de380abb
kThieb <kthiebdev@gmail.com>
2020-09-09 15:30:44 +0200

    Updates

--- precache-manifest.d8af33ee10658a269c07d5cfdd29b845.js
+++ precache-manifest.d8af33ee10658a269c07d5cfdd29b845.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "75ebaa6926c6c98479c0f2afe958151e",
+    "revision": "41c0819b473ec75798f410d2f69adfeb",
     "url": "/pathfinding-visualizer/index.html"
   },
   {
-    "revision": "6f7b34e0d5ca8be0b29d",
+    "revision": "145bb66cfe116270b47d",
     "url": "/pathfinding-visualizer/static/css/main.fe13db0c.chunk.css"
   },
   {
-    "revision": "5b53e848af2ed6189cba",
-    "url": "/pathfinding-visualizer/static/js/2.7d01e08c.chunk.js"
+    "revision": "9054f5bd8462d8f44809",
+    "url": "/pathfinding-visualizer/static/js/2.1da034c2.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/pathfinding-visualizer/static/js/2.7d01e08c.chunk.js.LICENSE.txt"
+    "url": "/pathfinding-visualizer/static/js/2.1da034c2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6f7b34e0d5ca8be0b29d",
-    "url": "/pathfinding-visualizer/static/js/main.67a9c99a.chunk.js"
+    "revision": "145bb66cfe116270b47d",
+    "url": "/pathfinding-visualizer/static/js/main.1934c1c0.chunk.js"
   },
   {
     "revision": "a2b7021554e2339a14f1",
